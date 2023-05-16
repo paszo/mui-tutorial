b26e03cc71d3e3ed916fbc6e8cd82d515a5c4ec6
paszo <paszo2008@gmail.com>
2023-05-16 19:56:44 +0200

    save

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Stack, Button} from '@mui/material';
+import {Stack, Button, IconButton, ButtonGroup} from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 
 export const MuiButton = () => {
     return (
@@ -21,6 +22,20 @@ export const MuiButton = () => {
                 <Button variant="contained" size="small">Small</Button>
                 <Button variant="contained" size="medium">Medium</Button>
                 <Button variant="contained" size="large">Large</Button>
+            </Stack>
+            <Stack spacing={2} direction="row">
+                <Button variant="contained" startIcon={<SendIcon/>} disableRipple onClick={() => {console.log('hi')}}>Send</Button>
+                <Button variant="contained" endIcon={<SendIcon/>} disableElevation>Send</Button>
+                <IconButton aria-label="send" color="success" size="small">
+                    <SendIcon/>
+                </IconButton>
+            </Stack>
+            <Stack direction="row">
+                <ButtonGroup variant="contained" orientation="vertical" size="small" color="secondary" aria-label="alignment button group">
+                    <Button>Left</Button>
+                    <Button>Center</Button>
+                    <Button>Right</Button>
+                </ButtonGroup>
             </Stack>
         </Stack>
     );
