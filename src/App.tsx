b68f9035deacff44ec6a3648f1f083efb3c081b5
paszo <paszo2008@gmail.com>
2023-05-26 20:04:12 +0200

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {MuiLayout} from "./components/MuiLayout";
 import {MuiCard} from "./components/MuiCard";
 import {MuiAccordion} from "./components/MuiAccordion";
 import {MuiImageList} from "./components/MuiImageList";
+import {MuiNavbar} from "./components/MuiNavbar";
 
 function App() {
   return (
@@ -29,7 +30,8 @@ function App() {
         {/*<MuiLayout/>*/}
         {/*<MuiCard></MuiCard>*/}
         {/*<MuiAccordion/>*/}
-        <MuiImageList/>
+        {/*<MuiImageList/>*/}
+        <MuiNavbar></MuiNavbar>
     </div>
   );
 }
