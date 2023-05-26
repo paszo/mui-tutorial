b3b29c55d2a9e6455eb1c5cadd76e3214e04b1f8
paszo <paszo2008@gmail.com>
2023-05-26 08:14:22 +0200

    checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import {MuiTypography} from './components/MuiTypography';
-import {MuiButton} from './components/MuiButton';
-import {MuiTextFields} from "./components/MuiTextFields";
-import {MuiSelect} from "./components/MuiSelect";
+// import {MuiTypography} from './components/MuiTypography';
+// import {MuiButton} from './components/MuiButton';
+// import {MuiTextFields} from "./components/MuiTextFields";
+// import {MuiSelect} from "./components/MuiSelect";
 import {MuiRadioButton} from "./components/MuiRadioButton";
+import {MuiCheckbox} from "./components/MuiCheckbox";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         {/*<MuiButton/>*/}
         {/*<MuiTextFields/>*/}
         {/*<MuiSelect/>*/}
-        <MuiRadioButton/>
+        {/*<MuiRadioButton/>*/}
+        <MuiCheckbox/>
     </div>
   );
 }
